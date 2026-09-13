4b3a55c18d58637ae7b579df919228fc4fc1c812
marcusffh <Marcusffh@gmail.com>
2026-09-13 14:23:00 +0200

    .

--- a1/src/APL/Eval.hs
+++ a1/src/APL/Eval.hs
@@ -152,7 +152,7 @@ eval env (Apply e1 e2) = --e1 is a function expression, e2 is an argument expres
 eval env (TryCatch e1 e2) =
   case eval env e1 of
     Left err -> eval env e2
-    Right Val -> Right Val
+    Right val -> Right val
 
 
 
