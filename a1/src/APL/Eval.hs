69a7b2501d7a6c700238be6b60f40d357237f59a
Ands081 <146835521+Ands081@users.noreply.github.com>
2026-09-13 10:35:55 +0200

    Update Eval.hs

--- a1/src/APL/Eval.hs
+++ a1/src/APL/Eval.hs
@@ -147,13 +147,12 @@ eval env (Apply e1 e2) = --e1 is a function expression, e2 is an argument expres
           eval (envExtend param argVal funEnv) body
     Right _ -> Left "valFun isnt a function" -- the case where valFun does not return a function
 
--- envExtend takes a variable name, a value, and an environment, and adds the variable name and value to the environment
 
 
--- eval env (TryCatch e1 e2) =
---  case eval env e1 of
---  Left err -> eval env e2
---  Right Val -> Right Val
+eval env (TryCatch e1 e2) =
+  case eval env e1 of
+    Left err -> eval env e2
+    Right Val -> Right Val
 
 
 
